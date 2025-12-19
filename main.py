974a8e98fe3825f6b0abdc20a56bbc4179bd718c
balaji <rbalajis25@gmail.com>
2025-12-19 02:08:40 +0000

    Scripted auto-commit on change (2025-12-19 02:08:40) by gitwatch.sh

--- main.py
+++ main.py
@@ -504,7 +504,7 @@ def step9():
         subs_dict["α65"] = alpha[5][4] 
         for i in range(8):
             for j in range(8):
-                if (i, j) not in [(1, 0), (4, 1), (5, 4), (0, 0), (1,0)]:
+                if (i, j) not in [(1, 0), (4, 1), (5, 4)]:
                     subs_dict[f"α{i + 1}{j + 1}"] = jnp.float64(0)
 
         for i in range(8):
