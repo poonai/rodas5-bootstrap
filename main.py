6134ee9d180e764a55b14b9d1d158b618a2fa012
balaji <rbalajis25@gmail.com>
2025-12-19 04:12:42 +0000

    Scripted auto-commit on change (2025-12-19 04:12:42) by gitwatch.sh

--- main.py
+++ main.py
@@ -408,7 +408,6 @@ def step9():
         for i in range(6):
             alpha = alpha.at[6, i].set(beta[5][i])
         
-        print(beta)
         w = jnp.linalg.inv(beta)
 
     def substitute_symbols(equation):
